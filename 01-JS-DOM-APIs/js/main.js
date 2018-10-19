354feae904c0e0c66651078e5632bf367fa3a7c4
franrai91 <raimondifran91@gmail.com>
2018-10-19 16:15:20 -0300

    Ejercicio 3

--- 01-JS-DOM-APIs/js/main.js
+++ 01-JS-DOM-APIs/js/main.js
@@ -6,3 +6,41 @@ function hacerFadeIn(){
 function mensajeAlerta(){
     alert("Mensaje de alerta!");
 }
+function mostrarChiste(){
+    var configurar = {};
+    configurar.method = 'GET';
+    configurar.url = 'http://api.icndb.com/jokes/random';
+
+    reusable(configurar).then(response =>{
+        var objeto = JSON.parse(response);
+        
+        if(objeto.type === "success"){
+            document.getElementById("contenidoJson").innerHTML = objeto.value.joke;
+        }
+    })
+    .catch(error => {
+        document.getElementById("contenidoJson").style.background = "red";
+    })
+}
+function reusable(configuracion) {
+	return new Promise(function (resolve,reject){
+		var httpRqs = new XMLHttpRequest;
+	    httpRqs.open(configuracion.method, configuracion.url);
+        httpRqs.onload = function() {
+          
+        if (httpRqs.status == 200) {
+            resolve(httpRqs.response);
+        }
+        else {       
+            reject(Error(httpRqs.statusText));
+        }
+        }
+      
+        httpRqs.onerror = function() {
+            reject(Error("Hubo un error"));
+        }
+        
+        httpRqs.send();
+    })  
+};
+
