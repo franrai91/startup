282e5f0582ec6ecdd1e1b829862ea4e1ad34e231
franrai91 <raimondifran91@gmail.com>
2018-10-19 00:40:17 -0300

    Ejercicio 1
    Los 4 puntos del ejercicio 1

---
+++ 01-JS-DOM-APIs/js/main.js
@@ -0,0 +1,8 @@
+function hacerFadeIn(){
+    var elementFadeIn = document.getElementById("fadein");
+    elementFadeIn.classList.remove("ocultado");
+    elementFadeIn.classList.add("mostrado");
+}
+function elboton(){
+    alert("Mensaje de alerta!");
+}
