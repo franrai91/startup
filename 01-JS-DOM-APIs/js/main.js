8f0f91b1582e9d9b6b39fcdf9efeaaa403c18338
franrai91 <raimondifran91@gmail.com>
2018-10-23 10:43:06 -0300

    Correcciones
    -Cambie la refencia de estilo y lo pase a css
    -Arregle la indentacion
    -Cambie el nombre de algunas variables y funciones(para intentar hacerlas mas claras)
    -Arregle la pisada de parametro value
    -Solucione la declaracion de i y n en la funcion

--- 01-JS-DOM-APIs/js/main.js
+++ 01-JS-DOM-APIs/js/main.js
@@ -3,48 +3,44 @@ function hacerFadeIn(){
     elementFadeIn.classList.remove("ocultado");
     elementFadeIn.classList.add("mostrado");
 }
-function mensajeAlerta(){
+function popUpMessage(){
     alert("Mensaje de alerta!");
 }
 function mostrarChiste(){
-    var configurar = {};
-    configurar.method = 'GET';
-    configurar.url = 'http://api.icndb.com/jokes/random';
+    var direc = {};
+    direc.method = 'GET';
+    direc.url = 'http://api.icndb.com/jokes/random';
 
-    reusable(configurar).then(response =>{
-        var objeto = JSON.parse(response);
+    reusable(direc).then(response =>{
+        var res = JSON.parse(response);
         
-        if(objeto.type === "success"){
-            document.getElementById("contenidoJson").innerHTML = objeto.value.joke;
+        if(res.type === "success"){
+            document.getElementById("contenidoJson").innerHTML = res.value.joke;
         }
     })
     .catch(error => {
-        document.getElementById("contenidoJson").style.background = "red";
+        document.getElementById("contenidoJson").classList.add("classError");
     })
 }
 function reusable(configuracion) {
 	return new Promise(function (resolve,reject){
 		var httpRqs = new XMLHttpRequest;
 	    httpRqs.open(configuracion.method, configuracion.url);
-        httpRqs.onload = function() {
-          
-        if (httpRqs.status == 200) {
-            resolve(httpRqs.response);
+        httpRqs.onload = function() {  
+            if (httpRqs.status == 200) {
+                resolve(httpRqs.response);
+            }
+            else {       
+                reject(Error(httpRqs.statusText));
+            }
         }
-        else {       
-            reject(Error(httpRqs.statusText));
-        }
-        }
-      
         httpRqs.onerror = function() {
             reject(Error("Hubo un error"));
         }
-        
         httpRqs.send();
     })  
 };
 function traerConParametros(value){
-    value = document.getElementById("valorUsuario").value;
     var configurar = {};
     configurar.method = 'GET';
     configurar.url = 'https://api.github.com/search/repositories?q=' + value;
@@ -60,17 +56,19 @@ function traerConParametros(value){
     })
     .catch(error => alert("cuidadooo"));    
 };
-function crearMatriz(matriz = [[14,25,78,2], [76,35,48], [98,37,81,88,91], [95,25,13]]){
+function crearMatriz(matriz = [[14,25,78,2], [76,35,48], [98,37,81,88,91], [95,25,13,285]]){
     var seccion = document.getElementById("matriz");
     var tablaGeneral = document.createElement('table');
     var cuerpoTabla = document.createElement('tbody');
 
     var largo = matriz.length;
-    for(i=0;i<largo;i++){
+    var i =0;
+   
+    for(i;i<largo;i++){
         var fila = document.createElement('tr');
         var elemento = matriz[i].length;
-
-        for(n=0;n<elemento;n++){
+        var n = 0;
+        for(n;n<elemento;n++){
             var columna = document.createElement('td');
             var node = document.createTextNode(matriz[i][n]);
 
