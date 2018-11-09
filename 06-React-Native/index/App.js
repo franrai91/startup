750472c12c0ca3a3a2a24598ff433d1d4a05a359
franrai91 <raimondifran91@gmail.com>
2018-11-09 11:10:08 -0300

    Topic 1 ejercicio 3

--- 06-React-Native/index/App.js
+++ 06-React-Native/index/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{
-        flexDirection: 'row',
-        height: 100,
-        padding: 20,
-      }}>
+      <ScrollView horizontal={true} style={{flexDirection: 'row',height: 100,padding: 20,}}>
         <View style={styles.newView}>
-        
         </View>
         <View style={styles.anotherView}>
-          
         </View>
-      </View>
+        <View style={styles.thirdView}>
+        </View>
+        <View style={styles.anotherView}>
+        </View>
+      </ScrollView>
       
     );
   }
@@ -23,7 +22,8 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   newView: {
-    flex: 1,
+    flex: 0.2,
+    height: 140,
     width: 340,
     backgroundColor: '#DC3939',
     fontSize: 15,
@@ -33,8 +33,19 @@ const styles = StyleSheet.create({
   },
   anotherView: {
     flex: 0.5,
+    height: 240,
     width: 240,
     backgroundColor: 'blue',
+    fontSize: 15,
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderColor: 'black',
+  },
+  thirdView:{
+    flex: 0.3,
+    height: 540,
+    width: 20,
+    backgroundColor: 'grey',
     fontSize: 15,
     alignItems: 'center',
     borderStyle: 'solid',
