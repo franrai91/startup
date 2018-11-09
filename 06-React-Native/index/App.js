34eee6b9e8c5c1d2181dfe9be358066716a67ad2
franrai91 <raimondifran91@gmail.com>
2018-11-09 10:57:24 -0300

    Topic 1 Ejercicio 2

--- 06-React-Native/index/App.js
+++ 06-React-Native/index/App.js
@@ -4,8 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.newView}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{
+        flexDirection: 'row',
+        height: 100,
+        padding: 20,
+      }}>
+        <View style={styles.newView}>
+        
+        </View>
+        <View style={styles.anotherView}>
+          
+        </View>
       </View>
       
     );
@@ -14,9 +23,18 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   newView: {
-    margin: 60,
+    flex: 1,
     width: 340,
     backgroundColor: '#DC3939',
+    fontSize: 15,
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderColor: 'black',
+  },
+  anotherView: {
+    flex: 0.5,
+    width: 240,
+    backgroundColor: 'blue',
     fontSize: 15,
     alignItems: 'center',
     borderStyle: 'solid',
