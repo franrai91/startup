245b12b1008b38724a5cae9918d98de96f8e4803
franrai91 <raimondifran91@gmail.com>
2018-11-10 09:56:57 -0300

    Topic 3 ejercicio 2

--- 06-React-Native/topic3/App.js
+++ 06-React-Native/topic3/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props){
     super(props),
     this.state = {text: ''}
+    this.handleOnPress = this.handleOnPress.bind(this)
+  }
+  handleOnPress(){
+    this.setState({
+      text: ''
+    });
   }
   render() {
     return (
       <View style={styles.container}>
-        <TextInput placeholder="Write over here" onChange={(text)=> this.setState({text})}></TextInput>
-       
+        <TouchableOpacity style={styles.inputType} onPress={this.handleOnPress}>
+          <TextInput placeholder="Write over here" onChange={(text)=> this.setState({text})} value={this.state.text}></TextInput>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -23,4 +30,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputType:{
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  }
 });
