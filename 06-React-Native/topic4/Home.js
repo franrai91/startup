001568f1108406e27d39bb56569b4ee952622a26
franrai91 <raimondifran91@gmail.com>
2018-11-11 10:14:36 -0300

    Topic 4 ejercicio 4

--- 06-React-Native/topic4/Home.js
+++ 06-React-Native/topic4/Home.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import styles from './styles';
 import {createBottomTabNavigator} from 'react-navigation';
 import Form1 from './Form1';
+import { Ionicons } from '@expo/vector-icons';
 
 class Home extends React.Component {
   render() {
@@ -14,10 +15,22 @@ class Home extends React.Component {
   }
 }
 export default createBottomTabNavigator({
-  Home: Home,
-  Form: Form1,
+  Forms: Home,
+  Others: Form1,
 },{
-  
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Forms') {
+          iconName = 'ios-list';
+        } else if (routeName === 'Others') {
+          iconName = 'logo-apple';
+        }
+
+        return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+      },
+    }),
   tabBarOptions: {
       activeTintColor: '#000',
       inactiveTintColor: 'gray',
